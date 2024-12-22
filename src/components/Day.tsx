f6d412154a439d4758879eabb9b6a6c2d1b56bfb
Nik <playpaper12@gmail.com>
2024-12-23 00:52:56 +0200

    feat: added notification and day page protection

--- src/components/Day.tsx
+++ src/components/Day.tsx
@@ -1,39 +1,65 @@
 import s from "./Day.module.css";
 import cn from "classnames";
 import { useNavigate } from "react-router";
+import { useEffect, useRef } from "react";
 
 export type DayProps = {
   text: string;
-  id: string;
+  day: string;
   size?: "square" | "double" | "wide" | undefined;
   transparent?: boolean;
   color?: "white" | "gradient" | "dark-green";
 };
+
+const NOTIFICATION_DELAY = 3000;
 
 export const Day = ({
   size = "square",
   transparent,
   color,
   text,
-  id,
+  day,
 }: DayProps) => {
   const navigate = useNavigate();
+  const notificationRef = useRef<HTMLDivElement>(null);
+
+  const currentDay = new Date().getDate();
+  const isLockedDay = currentDay < Number(day);
+
+  useEffect(() => {
+    notificationRef.current?.setAttribute("popover", "manual");
+  }, []);
 
   const handleClick = () => {
-    navigate(`/day/${id}`);
+    if (isLockedDay) {
+      notificationRef.current?.showPopover();
+
+      setTimeout(() => {
+        notificationRef.current?.hidePopover();
+      }, NOTIFICATION_DELAY);
+      return;
+    }
+
+    console.log("clicked");
+    navigate(`/day/${day}`);
   };
 
   return (
-    <div
-      className={cn(s.container, s[size], {
-        [s.transparent]: transparent,
-        [s.gradient]: color === "gradient",
-        [s.white]: color === "white",
-        [s.darkGreen]: color === "dark-green",
-      })}
-      onClick={handleClick}
-    >
-      <p>{text}</p>
-    </div>
+    <>
+      <div className={s.notification} ref={notificationRef}>
+        The day has not yet come
+      </div>
+      <div
+        className={cn(s.container, s[size], {
+          [s.transparent]: transparent,
+          [s.gradient]: color === "gradient",
+          [s.white]: color === "white",
+          [s.darkGreen]: color === "dark-green",
+        })}
+        onClick={handleClick}
+      >
+        <p>{text}</p>
+      </div>
+    </>
   );
 };
