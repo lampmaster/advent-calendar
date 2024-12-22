1e95c12cab3db50a5a3e6c7784e3be8976bbc9d5
Nik <playpaper12@gmail.com>
2024-12-22 15:59:48 +0200

    move fonts to assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-function App() {
+import { Day } from "./components/Day.tsx";
+import s from "./App.module.css";
 
+function App() {
   return (
-    <>
-        <div>My calendar</div>
-    </>
-  )
+    <div className={s.container}>
+      <Day text={"23"} color={"white"} />
+      <Day text={"24"} />
+      <Day text={"25"} color={"white"} />
+      <Day text={"26 december"} size={"wide"} transparent />
+      <Day text={"27"} color={"dark-green"} />
+      <Day text={"28"} />
+      <Day text={"29"} color={"white"} />
+      <Day text={"30"} color={"white"} />
+      <Day text={"31 dec"} size={"double"} color={"gradient"} />
+    </div>
+  );
 }
 
-export default App
+export default App;
