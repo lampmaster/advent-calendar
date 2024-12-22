36d2212d7f799dd29bf98646eabec9351ef40b50
Nik <playpaper12@gmail.com>
2024-12-22 16:45:03 +0200

    added title

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,18 @@ import s from "./App.module.css";
 function App() {
   return (
     <div className={s.container}>
-      <Day text={"23"} color={"white"} />
-      <Day text={"24"} />
-      <Day text={"25"} color={"white"} />
-      <Day text={"26 december"} size={"wide"} transparent />
-      <Day text={"27"} color={"dark-green"} />
-      <Day text={"28"} />
-      <Day text={"29"} color={"white"} />
-      <Day text={"30"} color={"white"} />
-      <Day text={"31 dec"} size={"double"} color={"gradient"} />
+      <h1 className={s.title}>advent calendar</h1>
+      <div className={s.calendar}>
+        <Day text={"23"} color={"white"} />
+        <Day text={"24"} />
+        <Day text={"25"} color={"white"} />
+        <Day text={"26 december"} size={"wide"} transparent />
+        <Day text={"27"} color={"dark-green"} />
+        <Day text={"28"} />
+        <Day text={"29"} color={"white"} />
+        <Day text={"30"} color={"white"} />
+        <Day text={"31 dec"} size={"double"} color={"gradient"} />
+      </div>
     </div>
   );
 }
